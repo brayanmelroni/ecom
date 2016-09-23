4ac4da5721abb9fb5f059428e0ad3a8a0cd3fb45
Brayan Melroni <brayanmelroni1@gmail.com>
2016-09-23 14:58:09 +0100

    Update dbConfig.php

--- resources/backend/entities/dbConfig.php
+++ resources/backend/entities/dbConfig.php
@@ -3,8 +3,8 @@
         /**
         * Defining the properties needed to connect to the database. 
         */
-        defined("DB_HOST") ? null : define("DB_HOST","localhost");
-        defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
-        defined("DB_USER") ? null : define("DB_USER","brayanmelroni1");
+        defined("DB_HOST") ? null : define("DB_HOST","");
+        defined("DB_NAME") ? null : define("DB_NAME","");
+        defined("DB_USER") ? null : define("DB_USER","");
         defined("DB_PASS") ? null : define("DB_PASS","");
 ?>
